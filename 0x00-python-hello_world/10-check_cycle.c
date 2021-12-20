caf0a4027c11655e38bc4d428dccbe8a088e05e2
cryptolake <dhiadah@gmail.com>
2021-12-21 00:24:33 +0100

    added check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,20 +4,22 @@ int check_cycle(listint_t *list)
 {
 	listint_t *p = list, *pp = list;
 
+	if (!p->next)
+		return (1);
+
 	while (p)
 	{
 
 		while (pp != p)
 		{
 			if (p->next == pp)
-			{
 				return (1);
-			}
+
 			pp = pp->next;
 		}
 
-		pp = list;
 		p = p->next;
+		pp = list;
 	}
 
 	return (0);
