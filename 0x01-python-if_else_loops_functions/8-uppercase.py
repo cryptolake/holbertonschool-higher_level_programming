2c7fe2e35ad5aaa50bb00e0e57b15388465dfae7
cryptolake <dhiadah@gmail.com>
2021-12-21 19:11:31 +0100

    task 8 done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        print("{:c}".format((ord(str[i]) - 32) if (ord(str[i]) >= 97 and ord(str[i]) <= 122) else ord(str[i])),end="")
+    print()
