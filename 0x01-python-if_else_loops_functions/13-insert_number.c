dc733c20dcc9fd16cc20fa9ef5e4a54e0d709553
cryptolake <dhiadah@gmail.com>
2021-12-21 21:08:02 +0100

    task 13 done

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *p, *new, *temp;
+
+	if (!head)
+		return (NULL);
+
+	p = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	
+	if (!p)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (p->next)
+	{
+		if (p->n <= new->n && p->next->n >= new->n)
+		{
+			temp = p->next;
+			p->next = new;
+			new->next = temp;
+
+			return (new);
+		}
+
+		p = p->next;
+	}
+	p->next = new;
+	new->next = NULL;
+
+	return (new);
+}
