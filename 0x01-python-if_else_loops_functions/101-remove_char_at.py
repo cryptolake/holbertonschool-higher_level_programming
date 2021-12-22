bc2d32f5247da7c91d21be1541b225a99fd6cbf2
cryptolake <dhiadah@gmail.com>
2021-12-22 13:43:31 +0100

    fixed code style?

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    """Remove charatcter at index n of string."""
     s = list(str)
+
     if (n < 0 or n > len(str) - 1):
         return str
 
     for i in range(n, len(str) - 1):
         s[i] = s[i + 1]
+
     s[len(str) - 1] = ''
 
     return ''.join(s)
