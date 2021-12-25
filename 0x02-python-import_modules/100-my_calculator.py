1edbdf36bd56428e5044df06a9d36d928f34b449
cryptolake <dhiadah@gmail.com>
2021-12-25 01:21:36 +0100

    calculator done

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import calculator_1
+import sys
+
+
+def main():
+    argv = sys.argv
+    argv.pop(0)
+    argc = len(argv)
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if not argv[1] in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[0])
+    b = int(argv[2])
+    res = 0
+    sign = argv[1]
+
+    if sign == "+":
+        res = calculator_1.add(a, b)
+    elif sign == "-":
+        res = calculator_1.sub(a, b)
+    elif sign == "*":
+        res = calculator_1.mul(a, b)
+    elif sign == "/":
+        res = calculator_1.div(a, b)
+
+    print("{:d} {} {:d} = {:d}".format(a, sign, b, res))
+
+
+if __name__ == "__main__":
+    main()
