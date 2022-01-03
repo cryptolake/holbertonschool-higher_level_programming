03b4a05898c8f110de4db032a30bb8c525059520
cryptolake <dhiadah@gmail.com>
2022-01-03 23:18:38 +0100

    fixed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
+    if not my_list:
         return None
-    mx = 0
-    for x in my_list: 
+    mx = float('-inf')
+    for x in my_list:
         if x > mx:
             mx = x
     return mx
