94ec156d75c0b0f9bd1159e8ef9d5f693769ce4b
cryptolake <dhiadah@gmail.com>
2022-01-05 19:39:33 +0100

    task 2 done

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = []
+
+    for item in my_list:
+        if item not in uniq_list:
+            uniq_list.append(item)
+
+    return sum(uniq_list)
