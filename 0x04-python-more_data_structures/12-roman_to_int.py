2e92e4f335fa11aad65c8fbc7174c1211c896090
cryptolake <dhiadah@gmail.com>
2022-01-06 00:46:13 +0100

    fixed non string case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     roman_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     i = 0
     num = 0
+    if roman_string is not str(roman_string) or roman_string is None:
+        return 0
     while i < len(roman_string):
         now = roman_table[roman_string[i]]
         try:
