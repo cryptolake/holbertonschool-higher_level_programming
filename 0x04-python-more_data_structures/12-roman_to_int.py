73de607472d01aae9a11706e840b4edcc6512924
cryptolake <dhiadah@gmail.com>
2022-01-06 13:50:48 +0100

    refactored logic it works completely now

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                   'C': 100, 'D': 500, 'M': 1000}
     i = 0
     num = 0
     if roman_string is not str(roman_string) or roman_string is None:
         return 0
+
     while i < len(roman_string):
         now = roman_table[roman_string[i]]
-        try:
+
+        if i + 1 < len(roman_string):
             after = roman_table[roman_string[i + 1]]
-        except IndexError:
+        else:
             after = 0
+
         if after > now:
             num += after - now
             i += 2
-        elif after < now:
+        else:
             num += after + now
             i += 2
-        else:
-            num += now
-            i += 1
     return num
