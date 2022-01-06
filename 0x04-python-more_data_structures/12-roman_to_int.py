42592e9b874919eec159a959154d0638aab7081d
cryptolake <dhiadah@gmail.com>
2022-01-06 18:27:30 +0100

    Carthage has finally conquered roma

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    roman_table = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                   'C': 100, 'D': 500, 'M': 1000}
-    i = 0
+    roman_table = {'M': 1000, 	 'C': 100,     'X': 10,	    'I': 1,
+                   'MM': 2000,	 'CC': 200,	'XX': 20,      'II': 2,
+                   'MMM': 3000, 'CCC': 300, 	'XXX': 30,     'III': 3,
+                   'CD': 400,     'XL': 40, 	'IV': 4,
+                   'D': 500,      'L': 50, 	    'V': 5,
+                   'DC': 600,     'LX': 60, 	    'VI': 6,
+                   'DCC': 700,    'LXX': 70,	    'VII': 7,
+                   'DCCC': 800,    'LXXX': 80,	'VIII': 8,
+                   'CM': 900,     'XC': 90, 	    'IX': 9}
+
     num = 0
+    i = 0
     if roman_string is not str(roman_string) or roman_string is None:
         return 0
-
+    temp_str = ""
     while i < len(roman_string):
-        now = roman_table[roman_string[i]]
+        while i < len(roman_string):
+            temp_str += roman_string[i]
+            if temp_str not in roman_table:
+                temp_str = temp_str[:-1]
+                num += roman_table[temp_str]
+                temp_str = ""
+                break
+            i += 1
+    num += roman_table[temp_str]
 
-        if i + 1 < len(roman_string):
-            after = roman_table[roman_string[i + 1]]
-        else:
-            after = 0
-
-        if after > now:
-            num += after - now
-            i += 2
-        else:
-            num += after + now
-            i += 2
     return num
