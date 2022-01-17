78cb66f8ad088cb2680fdc8fd44333e68d64df06
cryptolake <dhiadah@gmail.com>
2022-01-17 16:18:15 +0100

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if (x <= 0):
+        return 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -7,4 +9,4 @@ def safe_print_list(my_list=[], x=0):
             print()
             return i
     print()
-    return i + 1
+    return x
