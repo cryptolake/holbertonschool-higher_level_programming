2fda686e42ee4242a1410110ede1be143e3ca1d2
cryptolake <dhiadah@gmail.com>
2022-01-18 20:07:54 +0100

    we can see the square now

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a square with attributes, methods and very safe."""
+
+
+class Square():
+    """Square with it's size and area."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        for x in range(0, self.__size):
+            for y in range(0, self.__size):
+                print("#", end="")
+            print()
+        if (self.__size == 0):
+            print()
