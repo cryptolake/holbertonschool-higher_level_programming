1491160037fab93e4ccf521c3b53376c0cdab5fa
cryptolake <dhiadah@gmail.com>
2022-01-18 19:50:25 +0100

    decorators are weird

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a square with attributes, methods and very safe."""
+
+
+class Square():
+    """Square with it's size and area."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
