16bb1cbde2f033417cdb669504aa4ce9bd495fe3
cryptolake <dhiadah@gmail.com>
2022-01-18 16:57:59 +0100

    fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             sum_list.append(0)
             print("out of range")
-            return sum_list
+            continue
         try:
             res = n1 / n2
         except ZeroDivisionError:
