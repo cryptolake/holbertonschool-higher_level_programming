5ef5c9f91563c46829cd7257ec9d42c75f2e1292
cryptolake <dhiadah@gmail.com>
2022-01-18 20:40:41 +0100

    Revert "test"
    This reverts commit 50e8433fdb08905168a567653d026a5ef148c8ff.

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,7 +6,11 @@ class Square():
     """Square with it's size and area."""
 
     def __init__(self, size=0):
-        self.__size = self.size(size)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     @property
     def size(self):
