02d439b2aca27ced339dc2dec5b70a03443e0ae6
cryptolake <dhiadah@gmail.com>
2022-01-24 17:24:38 +0100

    i got this so wrong

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,11 +6,7 @@ class Square():
     """Square with it's size and area."""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
