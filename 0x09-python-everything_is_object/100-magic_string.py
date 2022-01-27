e68f1899e99f283d6faf136bc1a44f98a40a82e9
cryptolake <dhiadah@gmail.com>
2022-01-27 18:07:43 +0100

    readme

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-def a_func():
-    count = 0
-    st = "BestSchool"
-
-    def actualf():
-        nonlocal count
-        nonlocal st
-        for i in range(0, count):
-            st += ", BestSchool"
-        count += 1
-        return st
-    return actualf
-
-
-magic_string = a_func()
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
