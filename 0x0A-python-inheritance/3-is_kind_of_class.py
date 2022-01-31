4041a739f44761013611dbd450692687c3b6c1a9
cryptolake <dhiadah@gmail.com>
2022-01-31 18:22:09 +0100

    fixed docstyle

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""check if the object is an instance of, or if the object is an
+instance of a class that inherited from, the specified class."""
+
 
 def is_kind_of_class(obj, a_class):
+    """check if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class."""
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""check if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class."""
+
 
 def inherits_from(obj, a_class):
+    """check if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class."""
     return a_class in list(obj.__class__.__mro__)[1:]
