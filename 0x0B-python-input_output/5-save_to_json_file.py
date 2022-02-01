334be15721ee6ffdb8420e66b8d64c16407cbbfc
cryptolake <dhiadah@gmail.com>
2022-02-01 18:09:53 +0100

    better json file handling

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """object to json file."""
     with open(filename, mode="w") as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,6 +6,5 @@ import json
 def load_from_json_file(filename):
     """json file to object."""
     with open(filename, mode="r") as f:
-        json_data = f.read()
-    json_data = json.loads(json_data)
+        json_data = json.load(f)
     return json_data
