bfb39a44e595e44d3e839660015c7ea3ec9ab976
cryptolake <dhiadah@gmail.com>
2022-02-01 17:11:20 +0100

    object to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""object to json file."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """object to json file."""
+    with open(filename, mode="w") as f:
+        f.write(json.dumps(my_obj))
+        f.close()
