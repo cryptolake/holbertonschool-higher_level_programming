5bd1b520e9597751a09d9915a218ddb2ac814519
cryptolake <dhiadah@gmail.com>
2022-02-02 00:43:34 +0100

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Pascal triangle."""
+
+
+def pascal_triangle(n):
+    """Pascal triangle."""
+    tria = [[1]]
+    if (n <= 0):
+        return tria
+    for i in range(1, n):
+        tria.append([])
+        for j in range(0, i + 1):
+            if j == 0 or j == i:
+                tria[i].append(1)
+            else:
+                tria[i].append(tria[i-1][j-1] + tria[i-1][j])
+    return tria
