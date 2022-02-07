3be5ceeecaa18f14ab104060285a95b49b734177
cryptolake <dhiadah@gmail.com>
2022-02-07 21:40:45 +0100

    testin

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,12 +36,11 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
-        self.x = x
         self.y = y
+        super().__init__(id)
 
     def __str__(self):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"\
