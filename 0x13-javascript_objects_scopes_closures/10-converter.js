021f23d97a08389d94241a21fd906307bce1a317
cryptolake <dhiadah@gmail.com>
2022-04-05 22:33:30 +0100

    fixed conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -8,7 +8,7 @@ exports.converter = function (base) {
       return (num % base).toString();
     }
     if (num % base >= 10) {
-      return String.fromCharCode(97 + (num % base) - 10);
+      return convFrom10(Math.floor(num / base)) + String.fromCharCode(97 + (num % base) - 10);
     }
     return convFrom10(Math.floor(num / base)) + (num % base).toString();
   }
