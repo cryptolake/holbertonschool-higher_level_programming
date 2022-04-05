7b5a6b46365c288960dc30d07de1c92dd6d6d651
cryptolake <dhiadah@gmail.com>
2022-04-05 22:42:14 +0100

    wow searching google is important

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   function convFrom10 (num) {
-    if (Math.floor(num / base) === 0) {
-      if (num % base >= 10) {
-        return String.fromCharCode(97 + (num % base) - 10);
-      }
-      return (num % base).toString();
-    }
-    if (num % base >= 10) {
-      return convFrom10(Math.floor(num / base)) + String.fromCharCode(97 + (num % base) - 10);
-    }
-    return convFrom10(Math.floor(num / base)) + (num % base).toString();
+    // if (Math.floor(num / base) === 0) {
+    //   if (num % base >= 10) {
+    //     return String.fromCharCode(97 + (num % base) - 10);
+    //   }
+    //   return (num % base).toString();
+    // }
+    // if (num % base >= 10) {
+    //   return convFrom10(Math.floor(num / base)) + String.fromCharCode(97 + (num % base) - 10);
+    // }
+    // return convFrom10(Math.floor(num / base)) + (num % base).toString();
+    // TURNS OUT ALL OF THIS IS UNNECESARRY HAHA
+    return num.toString(base);
   }
   return convFrom10;
 };
