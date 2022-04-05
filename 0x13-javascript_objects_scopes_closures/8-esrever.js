93fc93b6ce4a3dc569f5addcb5a1e376c4d5080d
cryptolake <dhiadah@gmail.com>
2022-04-05 21:18:25 +0100

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  rev = []
+  const rev = [];
 
   for (let i = list.length - 1; i >= 0; i--) {
-    rev[list.length - 1 - i] = list[i]
+    rev[list.length - 1 - i] = list[i];
   }
   return rev;
 };
