6c8a3b63666401090e91bb9441542b92be42f446
cryptolake <dhiadah@gmail.com>
2022-04-05 22:25:37 +0100

    convert from 10 to any base

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  function convFrom10 (num) {
+    if (Math.floor(num / base) === 0) {
+      if (num % base >= 10) {
+        return String.fromCharCode(97 + (num % base) - 10);
+      }
+      return (num % base).toString();
+    }
+    if (num % base >= 10) {
+      return String.fromCharCode(97 + (num % base) - 10);
+    }
+    return convFrom10(Math.floor(num / base)) + (num % base).toString();
+  }
+  return convFrom10;
+};
