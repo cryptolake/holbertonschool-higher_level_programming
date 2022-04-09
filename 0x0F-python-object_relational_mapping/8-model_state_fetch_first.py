31b2f7677c22c4cccd6eae875881d99abe43245c
cryptolake <dhiadah@gmail.com>
2022-04-09 04:41:50 +0100

    first state fetched

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""List first state using ORM."""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def main(user, passw, db):
+    """List first state in database and print result in order."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).first()
+    print("{:d}: {:s}".format(first_state.id, first_state.name))
+
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3])
