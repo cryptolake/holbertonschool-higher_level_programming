877f9c08b751be581761d5e2042b28c45c4b638b
cryptolake <dhiadah@gmail.com>
2022-04-21 14:30:26 +0100

    request name

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get body of request
-[[ $(curl -s -I "$1" | head -1 | cut -d' ' -f2) == "200" ]] && curl -s "$1"
+[[ $(curl -s -I "$1" | head -1 | cut -d' ' -f2) == "200" ]] && curl -s -X "GET" "$1"
