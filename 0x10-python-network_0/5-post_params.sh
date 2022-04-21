29ba1823bda95966d15ebc505950b4740fa870e9
cryptolake <dhiadah@gmail.com>
2022-04-21 21:09:29 +0100

    forgot silent option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Request methods for Post
-curl -X "POST" -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
+curl -s -X POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
