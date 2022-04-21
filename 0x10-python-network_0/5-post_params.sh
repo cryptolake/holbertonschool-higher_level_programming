5cd1cac45725f03129433df86dd5ed33b65405c4
cryptolake <dhiadah@gmail.com>
2022-04-21 21:01:42 +0100

    post paramters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Request methods for Post
-curl -X 'POST' "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -X "POST" -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
