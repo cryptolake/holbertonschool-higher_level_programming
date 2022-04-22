aa99eace504f621c7bafc0bb0efec0e8c1931e88
cryptolake <dhiadah@gmail.com>
2022-04-22 21:49:28 +0100

    only run as script

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,5 +3,6 @@
 from sys import argv
 import requests
 
-r = requests.get(argv[1])
-print(r.headers["X-Request-Id"])
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers["X-Request-Id"])
