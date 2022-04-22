dbba36576467ef6151bd8a45485699a2f0e0934f
cryptolake <dhiadah@gmail.com>
2022-04-22 21:00:24 +0100

    fixed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Get http header."""
+from sys import argv
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen(argv[1]) as response:
     print(response.getheader('X-Request-Id'))
