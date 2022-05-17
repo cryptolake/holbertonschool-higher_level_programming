94afc9202effbfa60157c7aa763bf8ecadbf9212
cryptolake <dhiadah@gmail.com>
2022-05-17 20:38:43 +0100

    learning js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const axios = require('axios');
+axios.get(process.argv[2]+'/?format=json')
+  .then(resp => {
+    let charCount = 0;
+    const films = resp.data.results;
+    const character = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i=0; i<films.length; i++) {
+      if (films[i].characters.indexOf(character)>-1) {
+        charCount += 1;
+      }
+    }
+    console.log(charCount);
+  });
