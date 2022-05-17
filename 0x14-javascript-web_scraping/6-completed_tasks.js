91e7a3c8f4391b3acf607e31642391cf5be9f461
cryptolake <dhiadah@gmail.com>
2022-05-17 22:00:17 +0100

    how many completed their tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+axios.get(process.argv[2])
+  .then(resp => {
+    const todos = resp.data;
+    let completed = {};
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].completed) {
+        if (completed[todos[i].userId] !== undefined) {
+          completed[todos[i].userId] += 1;
+        } else {
+          completed[todos[i].userId] = 1;
+        }
+      }
+    }
+    console.log(completed)
+  });
