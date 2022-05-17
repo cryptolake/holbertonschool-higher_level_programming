64b7114ced4a6d850533ef3c1be5cf5434ae64c3
cryptolake <dhiadah@gmail.com>
2022-05-17 18:48:12 +0100

    using axios

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const axios = require('axios');
+axios.get(process.argv[2])
+  .then(resp => {
+    console.log('code:', resp.status);
+  })
+  .catch(resp => {
+    console.log('code:', resp.response.status);
+  });
