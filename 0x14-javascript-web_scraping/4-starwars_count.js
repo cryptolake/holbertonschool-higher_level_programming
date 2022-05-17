00c78e159525bb2f980cbb250fd285e19bda676a
cryptolake <dhiadah@gmail.com>
2022-05-17 20:46:18 +0100

    correct way to get json data

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 const axios = require('axios');
-axios.get(process.argv[2]+'/?format=json')
+axios.get(process.argv[2], {
+  headers: {
+    Accept: 'application/json'
+  }
+})
   .then(resp => {
     let charCount = 0;
     const films = resp.data.results;
     const character = 'https://swapi-api.hbtn.io/api/people/18/';
-    for (let i=0; i<films.length; i++) {
-      if (films[i].characters.indexOf(character)>-1) {
+    for (let i = 0; i < films.length; i++) {
+      if (films[i].characters.indexOf(character) > -1) {
         charCount += 1;
       }
     }
